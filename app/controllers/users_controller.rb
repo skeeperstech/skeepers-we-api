1f0a924d279efd23299e242413bf1976344f7b80
Hijas <virus@localhost.localdomain>
2019-12-14 22:52:44 +0530

    user action created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :authenticate_request
+  skip_before_action :authenticate_request, :only => [:authenticate]
 
   def authenticate
     command = AuthenticateUser.call(params[:email], params[:password])
@@ -10,4 +10,13 @@ class UsersController < ApplicationController
       render json: { error: command.errors }, status: :unauthorized
     end
   end
+
+  def show_user
+    if @current_user
+      render json: { user: @current_user, status: 200}
+    else
+      render json: { error: "Something went wrog", status: 403 }
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get 'users/authenticate'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post 'authenticate', to: 'users#authenticate'
+  get 'show_user', to: 'users#show_user'
 end
